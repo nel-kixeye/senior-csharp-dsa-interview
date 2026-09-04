2c3455146ea428fefef14456888fb0ffdaee1e5c
Nelson Altarejos <nel.kixeye@gmail.com>
2026-09-04 11:23:08 -0700

    Add Dictionary exercise tests

--- BOOKSHELF/Exercises/Code/Book02-Dictionary/E02.1-TwoSum/Tests.cs
+++ BOOKSHELF/Exercises/Code/Book02-Dictionary/E02.1-TwoSum/Tests.cs
@@ -1,24 +1,22 @@
-public static class Tests
+using Xunit;
+
+public class TwoSumTests
 {
-    /*
-     * Test Case 1
-     * Input:    nums = { 2, 7, 11, 15 }, target = 9
-     * Expected: { 0, 1 }
-     *
-     * Test Case 2 - the two values are equal
-     * Input:    nums = { 3, 3 }, target = 6
-     * Expected: { 0, 1 }
-     *
-     * Test Case 3 - answer is not at the start
-     * Input:    nums = { 3, 2, 4 }, target = 6
-     * Expected: { 1, 2 }
-     *
-     * Test Case 4 - negatives
-     * Input:    nums = { -3, 4, 3, 90 }, target = 0
-     * Expected: { 0, 2 }
-     *
-     * Test Case 5 - must not use the same element twice
-     * Input:    nums = { 3, 2, 4 }, target = 6
-     * Expected: NOT { 0, 0 }
-     */
+    [Theory]
+    [InlineData(new[] { 2, 7, 11, 15 }, 9, new[] { 0, 1 })]
+    [InlineData(new[] { 3, 3 }, 6, new[] { 0, 1 })]
+    [InlineData(new[] { 3, 2, 4 }, 6, new[] { 1, 2 })]
+    [InlineData(new[] { -3, 4, 3, 90 }, 0, new[] { 0, 2 })]
+    public void TwoSum_ReturnsSpecifiedIndices(int[] nums, int target, int[] expected)
+        => Assert.Equal(expected, Solution.TwoSum(nums, target));
+
+    [Fact]
+    public void TwoSum_DoesNotReuseTheSameElement()
+    {
+        var nums = new[] { 3, 2, 4 };
+        var result = Solution.TwoSum(nums, 6);
+        Assert.Equal(2, result.Length);
+        Assert.NotEqual(result[0], result[1]);
+        Assert.Equal(6, nums[result[0]] + nums[result[1]]);
+    }
 }
